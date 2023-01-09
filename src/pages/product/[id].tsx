9f661a267d93f1a6cd528a8371865a83d21ec94e
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-01-09 15:47:01 -0300

    feat: page product details

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
