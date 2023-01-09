c50fb4a72caea30bd91f74d39035f6277f1f31de
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-01-09 14:13:17 -0300

    picking up products from the stripe

--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@
 const nextConfig = {
     reactStrictMode: true,
 
+    images: {
+        domains: [
+            'files.stripe.com'
+        ]
+    },
+
     experimental: {
         images: {
             allowFutureImage: true,
-        }
-    }
+        },
+    },
 }
 
 module.exports = nextConfig
